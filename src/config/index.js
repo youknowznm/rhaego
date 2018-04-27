e4628716f41f936f55a19cc2d9a19793ba6d6a73
youknowznm <znm92@icloud.com>
2018-04-28 01:15:25 +0800

    upd avatar

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const siteName = 'Material Blog'
 
 export const socialInfo = {
-  wechatQr: 'https://wx3.sinaimg.cn/mw1024/005Pjl1Cly1fp42gno68tj30g90g90ul.jpg',
+  wechatQr: 'https://wx3.sinaimg.cn/mw1024/005Pjl1Cly1fqrpq0emb2j30of0ofn03.jpg',
   email: 'zhngnmng@163.com',
   zhihu: 'https://www.zhihu.com/people/youkonwznm',
   github: 'https://github.com/youknowznm',
