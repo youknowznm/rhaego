823fcfc6403c3ae14b9834668056d64e42310f8d
youknowznm <znm92@icloud.com>
2021-04-16 15:06:55 +0800

    upd md style

--- frontend/src/modules/Article/index.js
+++ frontend/src/modules/Article/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import c from 'classnames'
-import ReactMarkdown from 'react-markdown'
+import marked from 'marked'
+import hljs from "highlight.js";
 
 import {ajax} from "~/utils";
 
@@ -15,8 +16,32 @@ export default class Article extends React.Component {
     markdownContent: ''
   }
 
-  componentDidMount() {
+  docRef = null
+  setDocRef = ref => {
+    this.docRef = ref
+  }
 
+  setHTML = () => {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      pedantic: false,
+      sanitize: false,
+      tables: true,
+      breaks: true,
+      smartLists: true,
+      smartypants: true,
+      highlight: code => {
+        return hljs.highlightAuto(code).value;
+      }
+    })
+    return {
+      __html: marked(this.state.markdownContent)
+    }
+  }
+
+  componentDidMount() {
+    this.setDocRef()
     ajax('http://localhost:3000', 'GET')
       .then(res => {
         this.setState({
@@ -25,12 +50,14 @@ export default class Article extends React.Component {
       })
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+  }
+
   render() {
 
     return (
-      <div className={'rhaego-markdown'} style={style}>
-        <ReactMarkdown linkTarget={'_blank'}
-        >{this.state.markdownContent}</ReactMarkdown>
+      <div className={'rhaego-markdown'} style={style} ref={ref => this.setDocRef(ref)}>
+        <div dangerouslySetInnerHTML={this.setHTML()}></div>
       </div>
     )
   }
