e45b2e429158e36f2510c74a22a78fbf4a540f85
youknowznm <znm92@icloud.com>
2021-05-02 20:59:58 +0800

    shell 修改

--- build.sh
+++ build.sh
@@ -4,22 +4,22 @@ echo "-------- 拉取远端仓库 --------"
 git pull
 
 echo "-------- npm ci --------"
-run "rm -rf node_modules"
-run "npm ci"
+rm -rf node_modules
+npm ci
 
 echo "-------- 前端构建 --------"
-run 'npm run build'
+npm run build
 
 echo "-------- 检查 secret.json --------"
 if [ ! -f "backend/secret.json" ]; then
-  run "cp -r backend/secret.json.example backend/secret.json"
+  cp -r backend/secret.json.example backend/secret.json
   echo "-------- 请在 backend/secret.json 维护账户. --------"
 fi
 
 echo "-------- 服务启动 --------"
-run 'pm2 stop backend/server'
-run 'pm2 start backend/server'
+pm2 stop backend/server
+pm2 start backend/server
 
-run 'nginx -s reload'
+nginx -s reload
 
 echo "-------- rhaego 已就绪. --------"
