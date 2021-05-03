7f62f2779889f27c383809a564248c64f262b040
youknowznm <znm92@icloud.com>
2021-05-03 12:36:42 +0800

    npm & scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 printf "\n-------- 拉取远端仓库 --------\n"
-git pull
+git pull --ff-only
 
 printf "\n-------- npm ci --------\n"
 rm -rf node_modules
@@ -12,23 +12,23 @@ printf "\n-------- 前端构建 --------\n"
 npm run build
 
 printf "\n-------- 检查 secret.json --------\n"
-if [ ! -f "backend/secret.json" ]; then
+if [ ! -f "./backend/secret.json" ]; then
   cp -r backend/secret.json.example backend/secret.json
 fi
 
 printf "\n-------- 检查 resume.md --------\n"
-if [ ! -f "backend/data/resume.md" ]; then
-  cp -r backend/data/resume.md.example backend/data/resume.md
+if [ ! -f "./backend/data/resume.md" ]; then
+  cp -r ./backend/data/resume.md.example backend/data/resume.md
 fi
 
 printf "\n-------- 检查文件服务目录 --------\n"
-if [ ! -d "backend/files" ]; then
-  mkdir "backend/files"
+if [ ! -d "./backend/files" ]; then
+  mkdir "./backend/files"
 fi
 
 printf "\n-------- 重启服务 --------\n"
-pm2 stop backend/server
-pm2 start backend/server
+pm2 stop ./backend/server
+pm2 start ./backend/server
 
 printf "\n-------- 重启 nginx --------\n"
 nginx -s reload
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,18 +10,18 @@ printf "\n-------- 前端构建 --------\n"
 npm run build
 
 printf "\n-------- 检查 secret.json --------\n"
-if [ ! -f "backend/secret.json" ]; then
-  cp -r backend/secret.json.example backend/secret.json
+if [ ! -f "./backend/secret.json" ]; then
+  cp -r ./backend/secret.json.example backend/secret.json
 fi
 
 printf "\n-------- 检查 resume.md --------\n"
-if [ ! -f "backend/data/resume.md" ]; then
-  cp -r backend/data/resume.md.example backend/data/resume.md
+if [ ! -f "./backend/data/resume.md" ]; then
+  cp -r ./backend/data/resume.md.example backend/data/resume.md
 fi
 
 printf "\n-------- 检查文件服务目录 --------\n"
-if [ ! -d "backend/files" ]; then
-  mkdir "backend/files"
+if [ ! -d "./backend/files" ]; then
+  mkdir "./backend/files"
 fi
 
 printf "\n-------- 初始化完成 --------\n"
